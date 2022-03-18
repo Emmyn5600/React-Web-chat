ee95f6d357a72ed61ab10544edd726b35aba2694
emmyn5600 <emmy66418@gmail.com>
2022-03-18 12:01:03 +0200

    Add a form for the chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+import { TextInput } from './component/TextInput';
+import { MessageLeft, MessageRight } from './component/Message';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+      paper: {
+      width: '80vw',
+      height: '80vh',
+      maxWidth: '500px',
+      maxHeight: '700px',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      position: 'relative',
+    },
+    paper2: {
+      width: '80vw',
+      maxWidth: '500px',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      position: 'relative',
+    },
+    container: {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    messagesBody: {
+      width: 'calc( 100% - 20px )',
+      margin: 10,
+      overflowY: 'scroll',
+      height: 'calc( 100% - 80px )',
+    },
+  }));
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <Paper className={classes.paper} zDepth={2}>
+        <Paper id="style-1" className={classes.messagesBody}>
+          <MessageLeft
+            message=""
+            timestamp="MM/DD 00:00"
+            photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+            displayName=""
+            avatarDisp={true}
+          />
+          <MessageLeft
+            message=""
+            timestamp="MM/DD 00:00"
+            photoURL=""
+            displayName="テスト"
+            avatarDisp={false}
+          />
+          <MessageRight
+            message=""
+            timestamp="MM/DD 00:00"
+            photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+            displayName="まさりぶ"
+            avatarDisp={true}
+          />
+          <MessageRight
+            message=""
+            timestamp="MM/DD 00:00"
+            photoURL="https://lh3.googleusercontent.com/a-/AOh14Gi4vkKYlfrbJ0QLJTg_DLjcYyyK7fYoWRpz2r4s=s96-c"
+            displayName="まさりぶ"
+            avatarDisp={false}
+          />
+        </Paper>
+        <TextInput />
+      </Paper>
     </div>
   );
 }
-
-export default App;
